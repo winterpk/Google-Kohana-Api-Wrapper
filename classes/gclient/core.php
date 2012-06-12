6b8a6401bdc832e43e65ff7cf86b2af006c9ced9
winterpk <winterpk@arr.ae>
2012-06-12 13:56:55 -0700

    core update

--- classes/gclient/core.php
+++ classes/gclient/core.php
@@ -188,32 +188,6 @@ class gclient_core
     }
 	
 	/**
-	 * Starts the web server application authentication workflow
-	 * This method ties into gclient controller which is passed as the redirect_uri 
-	 * Gclient controller action_index method is responsible for handling the 
-	 * authentication dance between Google server and our your application.
-	 * 
-	 * In short, this method will do everything possible to acquire a valid token
-	 * 
-	 * @example	 
-	 * 			Gclient::instance()->authenticate();
-	 * 			// continue konwing that you have a valid token 
-	 * @return 	true on successfull token validation
-	 */
-	public function authenticate()
-	{
-		if ($gtoken = Session::instance()->get('gtoken'))
-		{
-			$check = $this->validate_token($gtoken);
-			if ($check) // token is valid 
-			{
-				return true;
-			}
-		}
-		Request::$current->redirect(self::$_instance->auth_url());
-	}
-	
-	/**
 	 * Alias for base createAuthUrl() method
 	 *
 	 * @return string	auth url for user authentication
